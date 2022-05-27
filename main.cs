d04d67e71c38df758c1fbb24860e2c9d1224a5da
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-27 16:34:42 +0000

    cs

--- main.cs
+++ main.cs
@@ -1,7 +1,34 @@
 using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static async Task Main()
+    {
+
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(
+            "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+        );
+        // Console.WriteLine(response);
+        var jsonAsDictionary = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        // Console.WriteLine(jsonAsDictionary);
+        Console.WriteLine("");
+        JsonNode forecastNode = JsonNode.Parse(response)!;
+        // Console.WriteLine(forecastNode);
+        JsonNode weatherNode = forecastNode!["main"]!;
+        // Console.WriteLine(weatherNode);
+        JsonNode temperatureNode = weatherNode!["temp"]!;
+        string tempString = temperatureNode.ToString();
+        double tempDouble = Convert.ToDouble(tempString);
+        tempDouble = tempDouble - 273.15;
+        Console.WriteLine("The temperature is " + tempDouble.ToString("0") + "°C");
+        Console.WriteLine("");
+        Console.WriteLine("\nDone.");
+    }
 }
